47d0e91421e956b6605616fec4b993bf881ef86f
Nirusan <shanrajah@hotmail.com>
2019-11-15 02:34:07 -0500

    removed unnecessary junk files

--- Client 445/src/data/PacketHandler.java
+++
@@ -1,9 +0,0 @@
-package data;
-
-import java.io.IOException;
-
-public abstract class PacketHandler {
-
-	public abstract void process(Packet packet) throws ClassNotFoundException, IOException;
-
-}
